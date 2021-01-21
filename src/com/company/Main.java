03ef10eb67b731ee7628dcb7251fd6af658c36ef
Artem Kim <ikiiimi215@gmail.com>
2021-01-21 14:41:22 +0200

    second commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,45 +3,23 @@ package com.company;
 import java.util.Arrays;
 
 public class Main {
-	public static void linearSearch(int [] arr,int key){
-		for (int i = 0; i < arr.length; i++){
-			if(arr[i]==key){
-				System.out.println("found number:"+arr[i]+'\n'+"index:"+i);
-			}
-			else{
-				System.out.print("The specified number was not found!!!");
-			}
-			break;
+
+
+	int convertBall(int ball){
+		int result =  0;
+		if (ball<60){
+			System.out.println("3");
 		}
+		else if (ball<75){
+			System.out.println("3");
+		}
+		else if (ball<90){
+			System.out.println("4");
+		}
+		else {
+			System.out.println("5");
+		}
+		return result;
 	}
 
-	public static int binarySearch(int [] arr,int key){
-		int low = 0 ;
-		int high = arr.length-1;
-		int mid;
-		while (low<=high){
-			mid = low+(high - low) /2;
-			if(key < arr[mid]){
-				high = mid - 1;
-			}else if(key > arr[mid]){
-				low = mid + 1;
-			}
-			else{
-				return mid;
-			}
-		}
-		return -1;
-	}
-	public static int makeNegative(final int x) {
-		if(x>0){
-			int y = x ;
-			y = y-(x+y);
-			return y;
-		}
-		return x;
-	}
-	public static void main(String[] args) {
-	    int [] arr = {4,3,5,5,6};
-		System.out.println(makeNegative( 0));
-    }
 }
